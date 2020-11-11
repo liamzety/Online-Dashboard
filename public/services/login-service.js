05896af0969d9968d957f63e7627d3315af5d487
liamzety <58632095+liamzety@users.noreply.github.com>
2020-11-11 11:20:47 +0200

    m

--- public/services/login-service.js
+++ public/services/login-service.js
@@ -4,15 +4,15 @@ export const loginService = {
     handleLogin,
     getUsers
 }
-
+const BASE_URL = "/"
 async function getUsers() {
-    const res = await fetch("http://localhost:3030/api/user")
+    const res = await fetch(`${BASE_URL}api/user`)
     const users = await res.json()
     return users
 }
 async function handleLogin(user) {
     try {
-        const res = await fetch("http://localhost:3030/api/auth/login", {
+        const res = await fetch(`${BASE_URL}api/auth/login`, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json , text/plain, */*',
